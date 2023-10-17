0600b78cbae3dec524b76f184a678c19c04d2f91
rdococ <rd606six@googlemail.com>
2023-10-17 13:30:26 +0100

    feat: add exponentiation operator

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -578,6 +578,18 @@ const operators = function (isInitialSetup, isStage, targetId, colors) {
                 </shadow>
             </value>
         </block>
+        <block type="operator_exponentiate">
+            <value name="NUM1">
+                <shadow type="math_number">
+                    <field name="NUM"/>
+                </shadow>
+            </value>
+            <value name="NUM2">
+                <shadow type="math_number">
+                    <field name="NUM"/>
+                </shadow>
+            </value>
+        </block>
         ${blockSeparator}
         <block type="operator_random">
             <value name="FROM">
